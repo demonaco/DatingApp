9314f99701b189fa512eb7668cd9e8fcf8f55e53
Matt DeMonaco <54197370+demonaco@users.noreply.github.com>
2021-03-06 18:52:51 -0800

    Created Account Service and injected it into navbar component
    Created Account Service and injected it into navbar component

--- API/Interfaces/ITokenService.cs
+++ API/Interfaces/ITokenService.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Entities;
 
 namespace API.Interfaces
 {
-    interface TokenService
+    interface ITokenService
     {
         string CreateToken(AppUser user);
     }
