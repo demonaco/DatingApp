ec0bec934ef4c782948dbfd6280f7ee61d4a7f92
Matt DeMonaco <54197370+demonaco@users.noreply.github.com>
2021-03-08 21:06:32 -0800

    Setting up event emitter for cancel button
    Setting up event emitter for cancel button

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
+  users: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -16,5 +18,14 @@ export class HomeComponent implements OnInit {
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
+
+  getUsers() {
+    this.http.get('https://localhost:44349/api/users').subscribe(users => this.users = users);
+  }
+
+  cancelRegisterMode(event: boolean) {
+   this.registerMode = event; 
+  }
+
 
 }
