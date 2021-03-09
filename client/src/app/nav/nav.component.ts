ae41fa1eb77948ab29e194a15d8410f0d54e0e33
Matt DeMonaco <54197370+demonaco@users.noreply.github.com>
2021-03-08 16:02:38 -0800

    Adding Home component and directives for toggle function
    Adding Home component and directives for toggle function

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,12 +10,10 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn: boolean;
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
   }
 
 
@@ -21,7 +21,6 @@ export class NavComponent implements OnInit {
   {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     })
@@ -31,14 +30,7 @@ export class NavComponent implements OnInit {
   logout()
   {
     this.accountService.logout();
-    this.loggedIn = false;
   }
 
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(user => {
-      this.loggedIn = !!user;
-    }, error => {
-      console.log(error);
-   })
-  }
+
 }
