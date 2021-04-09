e13428c217aba05a5f68ebcf5b45ac0e41b0bfc4
Matt DeMonaco <54197370+demonaco@users.noreply.github.com>
2021-04-09 12:32:40 -0700

    Creating response code for successful adding of photo
    Creating response code for successful adding of photo

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -42,7 +42,7 @@ namespace API.Controllers
         }
 
         // api/3
-        [HttpGet("{username}")]
+        [HttpGet("{username}", Name="GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
 
@@ -88,7 +88,10 @@ namespace API.Controllers
             user.Photos.Add(photo);
 
             if (await _userRepository.SaveAllAsync())
-                return _mapper.Map<PhotoDto>(photo);
+            {
+                return CreatedAtRoute("GetUser", new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+
+            }   
 
             return BadRequest("Problem with adding photo.");
         }
