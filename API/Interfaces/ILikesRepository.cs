3b059e2fb00e022636183db0d4683d1766f5d43b
Matt DeMonaco <54197370+demonaco@users.noreply.github.com>
2021-04-22 13:48:45 -0700

    Created LikesController and set up methods to get likes and add a like
    Created LikesController and set up methods to get likes and add a like

--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
 
 namespace API.Interfaces
 {
-    interface ILikesRepository
+    public interface ILikesRepository
     {
+        Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
+        Task<AppUser> GetUserWithLikes(int userId);
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+       
     }
 }
