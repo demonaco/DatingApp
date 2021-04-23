bd799be23ba21001c29947eed69e6b17314af35a
Matt DeMonaco <54197370+demonaco@users.noreply.github.com>
2021-04-22 19:04:04 -0700

    paginating the likes on the client
    paginating the likes on the client

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -93,13 +93,15 @@ export class MembersService {
     return this.http.post(this.baseUrl + '/likes' + username, {})
   }
 
-  getLikes(predicate) {
-    return this.http.get<Partial<Member[]>>(this.baseUrl + 'likes?predicate=' + predicate);
+  getLikes(predicate: string, pageNumber, pageSize) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('predicate', predicate)
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes', params);
   }
-
+  
   private getPaginatedResult<T>(url, params) {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
- 
+  
      return this.http.get<T>(url, { observe: 'response', params }).pipe(
        map(response => {
          paginatedResult.result = response.body;
