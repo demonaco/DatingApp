2071b71f2929632a09a09acc5da2df927ed44d24
Matt DeMonaco <54197370+demonaco@users.noreply.github.com>
2021-04-25 16:53:06 -0700

    Setting up routes and query params for tabs
    Setting up routes and query params for tabs

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -13,7 +13,7 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
-  @ViewChild('memberTabs') memberTabs: TabsetComponent;
+  @ViewChild('memberTabs', {static: true}) memberTabs: TabsetComponent;
   member: Member;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];  
@@ -25,6 +25,10 @@ export class MemberDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMember();
+
+    this.route.queryParams.subscribe(params => {
+      params.tab ? this.selectTab(params.tab) : this.selectTab(0);
+    })
 
     this.galleryOptions = [
       {
@@ -56,6 +60,10 @@ export class MemberDetailComponent implements OnInit {
       this.member = member;
       this.galleryImages = this.getImages();
       })
+    }
+
+    selectTab(tabId: number) {
+      this.memberTabs.tabs[tabId].active = true;
     }
 
     loadMessages() {
