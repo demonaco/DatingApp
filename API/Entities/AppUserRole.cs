1fb38f57ef21b7bb8ac454ad59c2801f9e030e99
Matt DeMonaco <54197370+demonaco@users.noreply.github.com>
2021-04-27 00:21:49 -0700

    Creating entities for Identity implementation
    Creating entities for Identity implementation

---
+++ API/Entities/AppUserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUserRole
+    {
+        public AppUser User { get; set; }
+        public AppUser Role { get; set; }
+
+    }
+}
