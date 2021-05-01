5cb442c3011ed6801bc4092cfec81dddb56a589a
Matt DeMonaco <54197370+demonaco@users.noreply.github.com>
2021-05-01 15:27:48 -0700

    Implementing Unit of Work pattern
    Implementing Unit of Work pattern

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -72,10 +72,7 @@ namespace API.Data
                 .ToListAsync();
         }
 
-        public async Task<bool> SaveAllAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
+    
 
         public void Update(AppUser user)
         {
---
+++ API/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; set; }
+        IMessageRepository MessageRepository { get; set; }
+        ILikesRepository LikesRepository { get; set; }
+        Task<bool> Complete();
+        bool HasChanges();
+    }
+}
